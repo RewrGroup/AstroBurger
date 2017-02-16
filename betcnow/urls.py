32df5330e0b88ccc40329e07d52df9fb7c5a515b
rewr <betcnow@gmail.com>
2017-02-15 23:19:41 -0400

    añadido check-out

--- betcnow/urls.py
+++ betcnow/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="betcnow/home.html"), name='home'),
     url(r'^play/$', views.play, name='play'),
     url(r'^gourl_callback.html/$', views.callback, name='callback'),
-    url(r'^payment/$', views.payment, name='payment'),
+    url(r'^check-out/$', views.checkout, name='check-out'),
 ]
--- betcnow/views.py
+++ betcnow/views.py
@@ -5,6 +5,7 @@ from .models import Profile, User, Jugada, Pote
 from registration.backends.default.views import ActivationView
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.template import RequestContext
 import md5hash
 
 
@@ -17,36 +18,48 @@ def play(request):
 
     for i in Jugada.objects.filter(pote=pote):
         lista_status.append(int(i.status))
-    variables.update({"lista_status": lista_status})
 
-    if request.method == 'POST':
-        jugadas = request.POST.getlist('jugadas[]')
-        for i in jugadas:
-            jugada = Jugada.objects.get(pote=pote, numero=i)
-            jugada.status = '2'
-            jugada.jugador = request.user
-            jugada.save()
-
+    variables.update({"lista_status": lista_status, 'pote': pote})
     return render(request, template, variables)
 
 
-def payment(request):
-    boxID = 8591
-    tipo_pago = 'jugada'
-    user = request.user.username
-    amountUSD = 0.1
-    orderID = 'pote1'
-    md5 = md5hash.hash(boxID, tipo_pago, amountUSD, user, orderID)
-    variables = {'boxID': boxID, 'tipo_pago': tipo_pago, 'amountUSD': amountUSD, 'user': user, 'orderID': orderID,
-                 'md5': md5}
-    return render(request, 'betcnow/pago.html', variables)
+def checkout(request):
+    if request.method == 'POST':
+        pote = Pote.objects.get(id=request.POST.get('pote', None))
+        jugadas_procesadas = []
+        jugadas_ocupadas = []
+        jugadas = request.POST.getlist('jugadas[]')
+        amountUSD = 0
+        for i in jugadas:
+            jugada = Jugada.objects.get(pote=pote, numero=i)
+            if jugada.status == '1':
+                jugada.status = '2'
+                jugada.jugador = request.user
+                jugada.save()
+                jugadas_procesadas.append(jugada)
+                amountUSD += pote.valor_jugada
+            else:
+                jugadas_ocupadas.append(jugada)
+
+        boxID = 8591
+        tipo_pago = 'jugada'
+        user = request.user.username
+        orderID = 'pot' + str(pote)
+        md5 = md5hash.hash(boxID, tipo_pago, amountUSD, user, orderID)
+        variables = {'boxID': boxID, 'tipo_pago': tipo_pago, 'amountUSD': amountUSD, 'user': user, 'orderID': orderID,
+                     'md5': md5, 'jugadas_procesadas': jugadas_procesadas, 'jugadas_ocupadas': jugadas_ocupadas}
+        if jugadas_ocupadas.__len__() > 0:
+            is_taken = True
+            variables.update({'is_take': is_taken})
+        return render(request, 'betcnow/pago.html', variables)
+    else:
+        return HttpResponse()
 
 
 
 @csrf_exempt
 def callback(request, *args, **kwargs):
     html = ""
-    Pote.objects.create(valor_jugada=2)
     if request.method == 'POST':
         p = Pote.objects.create(valor_jugada=1)
         p.save()
