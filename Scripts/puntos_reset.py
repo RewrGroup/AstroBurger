b622e007c2b25af50001bce67570db6d118dded9
rewr <betcnow@gmail.com>
2017-03-09 02:42:20 -0400

    Script puntos_reset ya funca

--- Scripts/puntos_reset.py
+++ Scripts/puntos_reset.py
@@ -1,5 +1,14 @@
-from django.utils import timezone
-from betcnow.models import Profile
+import os
+import sys
+sys.path.append('/home/rewr/AstroBurger')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'betc.settings'
+import django
+django.setup()
 
-if timezone.now().day == 9:
-    Profile.objects.all().update(puntos=0) 
+from betcnow.models import Profile
+from django.utils import timezone
+
+if timezone.now().day == 1:
+    Profile.objects.all().update(puntos=0)
+
+print("Reseteado")
