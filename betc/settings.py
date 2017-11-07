df2c83c8a94caea890552fcca5f18d3c8f37a2cd
rewr <betcnow@gmail.com>
2017-11-07 14:00:06 -0400

    password reset email fixed

--- betc/settings.py
+++ betc/settings.py
@@ -137,6 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'admin@betcnow.com'
